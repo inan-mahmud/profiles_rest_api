c16cb7c67eb46dcc8310753e110c41e17017e145
Inan Mahmud <inan.mahmud1992@gmail.com>
2020-10-28 19:04:35 +0600

    Demo Api View Added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format = None):
+        """Returns a list of APIVIEW features"""
+        an_apiview = [
+            'Django Framework',
+            'Django Rest Framework',
+            'Learning Backend sytem'
+        ]
+
+        return Response({'message':'Hello!','an_apiview':an_apiview})
